f0673938c6d2233691c5c11ff80439cb6248828a
devdave <devdave@ominian.net>
2024-03-03 14:18:34 -0700

    verify this works

---
+++ scripts/sa2panda.py
@@ -0,0 +1,21 @@
+import datetime as DT
+import pathlib
+import pandas as pd
+
+import IPython
+
+from lib import models
+from lib.application import Application
+from lib.api import API
+
+HERE = pathlib.Path(__file__).parent.parent
+
+
+app = Application(HERE, HERE / "events.sqlite3")
+api = API(app)
+
+stmt = models.Queries.BreakdownByConditionsStmt()
+
+df = pd.read_sql(sql=stmt, con=app.engine)
+
+print(df)
