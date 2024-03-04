38f7f71b81ac41578e5320c1686ad9e195e0fcf4
devdave <devdave@ominian.net>
2024-03-04 15:05:59 -0700

    cleanup

--- ui/vite.config.mjs
+++ ui/vite.config.mjs
@@ -13,13 +13,10 @@ export default defineConfig({
     setupFiles: './vitest.setup.mjs',
   },
   build: {
-    sourcemap: true,
+    sourcemap: false,
     rollupOptions:{
       input: {
             main: resolve(__dirname, 'index.html'),
-            tasks: resolve(__dirname, 'window_src/tasks/index.html'),
-            reports: resolve(__dirname, 'window_src/reports/index.html'),
-            manager: resolve(__dirname, 'window_src/manager/index.html')
       }
     }
   }
